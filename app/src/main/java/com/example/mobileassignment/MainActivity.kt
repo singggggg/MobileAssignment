8f916e19ac45f42242b5d1f6807412f9a141fdca
_AlvinLZY <alvinzhiyoong.limx@sophicautomation.com>
2022-10-02 14:37:13 +0800

    - update btmNav UI

--- app/src/main/java/com/example/mobileassignment/MainActivity.kt
+++ app/src/main/java/com/example/mobileassignment/MainActivity.kt
@@ -4,21 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import androidx.fragment.app.Fragment
-import com.example.mobileassignment.Fragment.HomePageFragment
+import androidx.fragment.app.FragmentContainer
+import com.example.mobileassignment.Fragment.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val recommendationFragment = RecommendationFragment()
+    private val mealPlanFragment = MealPlanFragment()
     private val homepageFragment = HomePageFragment()
+    private val menuFragment = MenuFragment()
+    private val cartFragment = CartFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottom_navigation.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.ic_home-> replaceFragment(homepageFragment)
 
+        btmNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.menu_recommendation-> replaceFragment(recommendationFragment)
+                R.id.menu_mealPlan-> replaceFragment(mealPlanFragment)
+                R.id.menu_homepage-> replaceFragment(homepageFragment)
+                R.id.menu_menu-> replaceFragment(menuFragment)
+                R.id.menu_cart-> replaceFragment(cartFragment)
             }
             true
         }
